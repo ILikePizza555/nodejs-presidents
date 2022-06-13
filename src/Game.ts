15eda6840da1ba012473b2aa57d8a2daaf13b9e1
Izzy Lancaster <avrisaac555@gmail.com>
2022-06-12 17:20:34 -0700

    Added trick and move to game

--- src/Game.ts
+++ src/Game.ts
@@ -1,12 +1,32 @@
-import type { Game } from "boardgame.io";
-import { Card, generateStandardDeck, splitDeck } from "./Cards";
+import type { Ctx, Game } from "boardgame.io";
+import { INVALID_MOVE } from "boardgame.io/core";
+import { Card, generateStandardDeck, splitDeck, Value } from "./Card";
 
 export interface PresidentsGameState {
+    trick: Value | null,
     hands: Card[][]
 }
 
 export const PresidentsGame: Game<PresidentsGameState> = {
     setup: (ctx) => ({
+        trick: null,
         hands: splitDeck(ctx.numPlayers, ctx.random.Shuffle(generateStandardDeck())),
     }),
+    moves: {
+        playCard: (G, ctx, id: number) => {
+            const card = getCurrentPlayerHand(G, ctx)[id];
+
+            if (G.trick !== null && G.trick > card.value) {
+                return INVALID_MOVE;
+            }
+
+            // G.trick is null or less than the card value
+            const [playedCard] = getCurrentPlayerHand(G, ctx).splice(id, 1);
+            G.trick = playedCard.value;
+        }
+    }
 };
+
+function getCurrentPlayerHand(G: PresidentsGameState, ctx: Ctx): Card[] {
+    return G.hands[ctx.currentPlayer];
+}
